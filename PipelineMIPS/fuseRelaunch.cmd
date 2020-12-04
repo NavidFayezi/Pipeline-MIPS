cc49c64684313d6de7d6f0c437afe82499990e94
NavidFayezi <navidfayezi.98@gmail.com>
2020-12-04 23:33:16 +0330

    gitignore is now working

--- PipelineMIPS/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "C:/Users/Markazi.co/Desktop/Computer architecture Lab/PipelineMIPS/MIPS_test_isim_beh.exe" -prj "C:/Users/Markazi.co/Desktop/Computer architecture Lab/PipelineMIPS/MIPS_test_beh.prj" "work.MIPS_test" "work.glbl" 
